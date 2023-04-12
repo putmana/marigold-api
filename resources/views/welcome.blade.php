14af459b162f77186e273e64846c64b5a33ee0e9
putmana <adam@ptmn.io>
2023-04-11 21:47:20 -0600

    Installed Laravel Breeze and scaffolded out API

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Marigold API</title>
-    </head>
-    <body>  
-        <h3>Marigolds...</h3>
-    </body>
-</html>
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Http\Resources\AlbumResource;
-use App\Http\Resources\AlbumTracksResource;
-use App\Http\Resources\PlaylistResource;
-use App\Http\Resources\PlaylistTracksResource;
-use App\Http\Resources\TrackResource;
-use App\Models\Album;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Playlist;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,27 +9,11 @@ use App\Models\Playlist;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('/playlist', function () {
-    return PlaylistResource::collection(Playlist::all());
-});
-
-Route::get('/playlist/{id}', function (string $id) {
-    return new PlaylistTracksResource(Playlist::find($id));
-});
-
-Route::get('/album', function () {
-    return AlbumResource::collection(Album::all());
-});
-
-Route::get('/album/{id}', function (string $id) {
-    return new AlbumTracksResource(Album::find($id));
 });
